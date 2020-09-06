08758c17e1a4db58abd159ed8aadc76e399070fb
JulietK <julietgisemba000@>
2020-09-06 12:44:49 +0300

    added the class

--- .sdkman/candidates/groovy/Caeser Cipher/src/main/java/CaeserCipher.java
+++ .sdkman/candidates/groovy/Caeser Cipher/src/main/java/CaeserCipher.java
@@ -1,2 +1,3 @@
+public class CaeserCipher {
 
- public class CaeserCipher{}
+}
---
+++ .sdkman/candidates/groovy/Caeser Cipher/src/main/java/redone.java
@@ -0,0 +1,84 @@
+public class redone {
+    public static void main(String[] args) {
+//        StringBuilder var = java("God", 4);
+//        System.out.println(var);
+//
+        StringBuilder var = decipher("Ksh",4);
+          System.out.println(var);
+
+
+    }
+
+    public static StringBuilder java(String word, int shift) {
+        if (shift > 26) {
+            shift = shift % 26;
+        } else if (shift < 0) {
+            shift = (shift % 26) + 26;
+        }
+        StringBuilder holder = new StringBuilder();
+        int WordLength = word.length();
+
+        for (int index = 0; index < WordLength; index++) {
+            char ch = word.charAt(index);
+
+            if (Character.isLetter(ch)) {
+                if (Character.isLowerCase(ch)) {
+                    char shiftedWord = (char) (ch + shift);
+                    if (shiftedWord > 'z') {
+                        holder.append((char) (ch - (26 - shift)));
+                    } else {
+                        holder.append(shiftedWord);
+                    }
+                } else if (Character.isUpperCase(ch)) {
+                    char shiftedWord = (char) (ch + shift);
+                    if (shiftedWord > 'Z') {
+                        holder.append((char) (ch - (26 - shift)));
+                    } else {
+                        holder.append(shiftedWord);
+                    }
+                } else {
+                    holder.append(ch);
+                }
+            }
+
+        }
+        return holder;
+    }
+
+    private static StringBuilder decipher(String word, int shift) {
+        if (shift > 26) {
+            shift = shift % 26;
+        } else if (shift < 0) {
+            shift = (shift % 26) + 26;
+        }
+        StringBuilder holder2 = new StringBuilder();
+        int WordLength2 = word.length();
+
+        for (int i = 0; i < WordLength2; i++) {
+            char ch = word.charAt(i);
+
+            if (Character.isLetter(ch)) {
+                if (Character.isLowerCase(ch)) {
+                    char shiftedWord2 = (char) (ch - shift);
+                    if (shiftedWord2 < 'a') {
+                        holder2.append((char) (ch + (26 + shift)));
+                    } else {
+                        holder2.append(shiftedWord2);
+                    }
+                } else if (Character.isUpperCase(ch)) {
+                    char shiftedWord2 = (char) (ch - shift);
+                    if (shiftedWord2 < 'A') {
+                        holder2.append((char) (ch + (26 + shift)));
+                    } else {
+                        holder2.append(shiftedWord2);
+                    }
+                } else {
+                    holder2.append(ch);
+                }
+
+            }
+            ;
+        }
+        return holder2;
+    }
+}
