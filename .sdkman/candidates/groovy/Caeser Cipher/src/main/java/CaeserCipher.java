8322b09c40c21bac004cb9e8196815274b83b753
JulietK <julietgisemba000@>
2020-09-06 12:47:56 +0300

    added the main method

--- .sdkman/candidates/groovy/Caeser Cipher/src/main/java/CaeserCipher.java
+++ .sdkman/candidates/groovy/Caeser Cipher/src/main/java/CaeserCipher.java
@@ -1,3 +1,11 @@
 public class CaeserCipher {
+    public static void main(String[] args) {
+//        StringBuilder var = java("God", 4);
+//        System.out.println(var);
+//
+        StringBuilder var = decipher("Ksh",4);
+        System.out.println(var);
 
+
+    }
 }
